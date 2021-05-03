4eff9b1cb6ff282ed00805f2be89cb3adc34320a
Anushka <anumittal4641@gmail.com>
2021-05-03 14:38:46 +0530

    cli set to use

---
+++ cmd/cobra/get.go
@@ -0,0 +1,28 @@
+package cobra
+
+import (
+	"fmt"
+
+	"github.com/anushkamittal20/secret"
+	"github.com/spf13/cobra"
+)
+
+var getCmd = &cobra.Command{
+	Use:   "get",
+	Short: "Gets a secret in your secret storage",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("key=", encodingKey)
+		v := secret.File(encodingKey, secretsPath())
+		key := args[0]
+		value, err := v.Get(key)
+		if err != nil {
+			fmt.Println("no value set")
+			return
+		}
+		fmt.Printf("%s = %s\n", key, value)
+	},
+}
+
+func init() {
+	RootCmd.AddCommand(getCmd)
+}
